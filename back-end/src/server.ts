74b74c243692a346b49663ff8dffa45698b1ebf4
Jhonatas <jhonatasoliveira021@gmail.com>
2023-01-14 10:02:25 -0300

    feat: mongoose configuration done

--- back-end/src/server.ts
+++ back-end/src/server.ts
@@ -1,2 +1,19 @@
-import express from "express";
+import "dotenv/config";
 import mongoose from "mongoose";
+
+import { app } from "./app";
+
+mongoose.set("strictQuery", true);
+
+const main = async (): Promise<typeof mongoose> => {
+  return mongoose.connect(
+    `mongodb+srv://jmdevbr:${process.env.PASSWORD_MONGODB_ATLAS}@shareenergyjhonatasmtes.wct5ksl.mongodb.net/?retryWrites=true&w=majority&ssl=true`
+  );
+};
+try {
+  main();
+  app.listen(2580, () => console.log("Aplicação rodando na porta 2580"));
+  console.log("Database conectado com sucesso");
+} catch (error) {
+  console.log(error);
+}
